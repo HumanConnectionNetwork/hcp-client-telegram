b37106c8421101f5b397bb9f7b859c63ceef8907
Human Connection Foundation <reddeconexionhumana@gmail.com>
2026-07-06 13:38:52 -0300

    Update bot.py

--- app/bot.py
+++ app/bot.py
@@ -1,1 +1,20 @@
+from telegram.ext import Application, CommandHandler
 
+from app.config import settings
+from app.conversation.start import start
+
+
+def main() -> None:
+    application = Application.builder().token(
+        settings.telegram_bot_token
+    ).build()
+
+    application.add_handler(CommandHandler("start", start))
+
+    print("HCP Telegram Client is running...")
+
+    application.run_polling()
+
+
+if __name__ == "__main__":
+    main()
